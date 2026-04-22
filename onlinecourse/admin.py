f7aba735cda0cc959a76ff128fca589ab9aa723f
ZeroTrace-Ops <3isowh9@gmail.com>
2026-04-23 02:11:31 +0400

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,3 +1,30 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Course, Lesson, Question, Choice, Submission
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['text']
+
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+    list_display = ['title']
+
+
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
