0b9c2bb655d4111f4ede7805d92263d34850fd74
ZeroTrace-Ops <3isowh9@gmail.com>
2026-04-23 02:39:37 +0400

    Update apps.py

--- onlinecourse/apps.py
+++ onlinecourse/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class OnlinecourseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'onlinecourse'
+
+    def ready(self):
+        import onlinecourse.admin
